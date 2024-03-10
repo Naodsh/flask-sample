a6eb839374a03e39613b13714de3b568d4c4a787
Naodsh <naodadanech1977@gmail.com>
2024-03-10 14:56:11 +0000

    tird commit

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
-
+    return render_template("login.html", text="Testing", user="Naod")
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign up</p>"
+    return render_template("sign_up.html")
